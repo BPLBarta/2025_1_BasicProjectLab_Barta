0938f2f41e7b0cd83c4ccdccf7b66dbaab99609b
sihyun <168539171+rshyun@users.noreply.github.com>
2025-05-25 18:49:53 +0900

    feat: Initial navigation bar update

--- app/src/main/java/com/example/barta/BartaAppMain.kt
+++ app/src/main/java/com/example/barta/BartaAppMain.kt
@@ -1,42 +1,52 @@
 package com.example.barta
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.*
 import androidx.compose.ui.unit.dp
+import com.example.barta.ui.theme.LocalBartaPalette
 
 @Composable
 fun BartaAppMain() {
     val navController = rememberNavController()
+    val color = LocalBartaPalette.current
     val items = listOf(
-        Screen.Home,
-        Screen.Dashboard,
-        Screen.Notifications
+        NavigationBar.Dashboard,
+        NavigationBar.Home,
+        NavigationBar.Profile
     )
 
     Scaffold(
         bottomBar = {
             BottomAppBar(
-                backgroundColor = MaterialTheme.colors.surface,
-                contentColor = MaterialTheme.colors.onSurface,
+                backgroundColor = color.backgroundGray1,
+                contentColor = color.textGray2,
+                modifier = Modifier.height(60.dp),
                 elevation = 8.dp
             ) {
-                items.forEach { screen ->
+                items.forEach { NavigationBar ->
                     BottomNavigationItem(
                         icon = {
                             Icon(
-                                painter = painterResource(id = screen.icon),
-                                contentDescription = screen.label
+                                painter = painterResource(id = NavigationBar.icon),
+                                contentDescription = NavigationBar.label,
+                                modifier = Modifier.size(20.dp)
                             )
                         },
-                        label = { Text(screen.label) },
-                        selected = navController.currentBackStackEntryAsState().value?.destination?.route == screen.route,
+                        label = {
+                            Text(
+                                text = NavigationBar.label,
+                                style = typography.caption)},
+                        selected = navController.currentBackStackEntryAsState().value?.destination?.route == NavigationBar.route,
                         onClick = {
-                            navController.navigate(screen.route) {
+                            navController.navigate(NavigationBar.route) {
                                 popUpTo(navController.graph.findStartDestination().id) {
                                     saveState = true
                                 }
@@ -51,17 +61,17 @@ fun BartaAppMain() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = Screen.Home.route,
+            startDestination = NavigationBar.Home.route,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(Screen.Home.route) {
-                HomeScreen(navController, modifier = Modifier.padding(innerPadding))
-            }
-            composable(Screen.Dashboard.route) {
+            composable(NavigationBar.Dashboard.route) {
                 DashboardScreen(navController, modifier = Modifier.padding(innerPadding))
             }
-            composable(Screen.Notifications.route) {
-                NotificationsScreen(navController, modifier = Modifier.padding(innerPadding))
+            composable(NavigationBar.Home.route) {
+                HomeScreen(navController, modifier = Modifier.padding(innerPadding))
+            }
+            composable(NavigationBar.Profile.route) {
+                ProfileScreen(navController, modifier = Modifier.padding(innerPadding))
             }
         }
     }
