a449a624ded6c7dba5377c5f64bfd6c60e4cdb28
sihyun <168539171+rshyun@users.noreply.github.com>
2025-05-25 18:27:16 +0900

    feat: Add custom color palette for app theme

--- app/src/main/java/com/example/barta/ui/theme/BartaTheme.kt
+++ app/src/main/java/com/example/barta/ui/theme/BartaTheme.kt
@@ -1,9 +1,10 @@
 package com.example.barta.ui.theme
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
@@ -12,10 +13,50 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.example.barta.R
 
-private val DarkColorPalette = darkColors(
-    primary = Color(0xFFBB86FC),
-    primaryVariant = Color(0xFF3700B3),
-    secondary = Color(0xFF03DAC6)
+data class BartaPalette(
+    val primaryOrange: Color,
+    val textWhite: Color,
+    val textBlack: Color,
+    val textGray1: Color,
+    val textGray2: Color,
+    val backgroundGray1: Color,
+    val backgroundGray2: Color,
+    val backgroundWhite: Color,
+    val recipeBoxGray: Color,
+    val backgroundOverlayOrange: Color,
+    val focusOverlayBlack50: Color,
+    val focusOverlayBlack60: Color,
+    val highlightRed: Color,
+    val dividerGray: Color,
+    val progress: Color,
+    val timer1: Color,
+    val timer2: Color,
+    val timerProgress: Color
+)
+
+val LocalBartaPalette = staticCompositionLocalOf {
+    BartaPalette(
+        primaryOrange = Color(0xFFFBAA73),
+        textWhite = Color(0xFFFFFFFF),
+        textBlack = Color(0xFF000000),
+        textGray1 = Color(0xFFBFBFBF),
+        textGray2 = Color(0xFF5C5C5C),
+        backgroundGray1 = Color(0xFFFBFBFB),
+        backgroundGray2 = Color(0xFFF2F2F2),
+        backgroundWhite = Color(0xFFFFFFFF),
+        recipeBoxGray = Color(0xFF7A7A7A),
+        backgroundOverlayOrange = Color(0xE6FCECD7),
+        focusOverlayBlack50 = Color(0x7F000000),
+        focusOverlayBlack60 = Color(0x99000000),
+        highlightRed = Color(0xFFC70000),
+        dividerGray = Color(0xFFE8E8E8),
+        progress = Color(0xFF00FF88),
+        timer1 = Color(0xFFD9D9D9),
+        timer2 = Color(0xFFA2A2A2),
+        timerProgress = Color(0xFFFF686B)
+    )
+}
+
 val suiteFont = FontFamily(
     Font(R.font.suite_light, FontWeight.Light),
     Font(R.font.suite_regular, FontWeight.Normal),
