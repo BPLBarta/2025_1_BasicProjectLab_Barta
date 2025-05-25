0ab68c0eca4bcdcd3eb823250aab17129d83bb66
sihyun <168539171+rshyun@users.noreply.github.com>
2025-05-26 05:30:05 +0900

    feat: initial login flow with database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
+    implementation(libs.androidx.annotation)
 
 
 
